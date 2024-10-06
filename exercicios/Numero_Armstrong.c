90679c65d79c5d362d90c84bc865ec27d1082ac9
JoaoPedroSalesDeuss <1447064@sga.pucminas.br>
2024-10-06 03:14:51 -0300

    nuemro de armstrong att

--- exercicios/Numero_Armstrong.c
+++ exercicios/Numero_Armstrong.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 
+int armstrong(int n)
+{
+    int original = n;
+    int tam = 0;
+    int soma = 0;
+
+    while (n != 0) {
+        n /= 10;
+        tam++;
+    }
+    
+    n = original;
+
+    while (n != 0) {
+        int digito = n % 10;
+        int potencia = 1;
+        for (int i = 0; i < tam; i++) {
+            potencia *= digito;
+        }
+        soma += potencia;
+        n /= 10;
+    }
+    return (soma == original);
+}
+
 int main()
 {
+    int n;
 
+    printf("Digite um numero para ver se e um numero de armstrong: ");
+    scanf("%d", &n);
 
+    int resultado = armstrong(n);
 
+    if(resultado == 1)
+    {
+        printf("E um numero de Armstrong!");
+    }
+    else
+    {
+        printf("Nao e um numero de Armstrong!");
+    }
 
     return 0;
 }
