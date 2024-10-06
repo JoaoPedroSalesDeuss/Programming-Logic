034c94f5b7f473bc9cb6f83247eb847c8bb1ab9c
JOAO PEDRO SALES DE DEUS <joaopedrosalesdedeuss@hotmail.com>
2024-10-06 00:07:12 -0300

    subindo mais um arquivo

--- ex basicos/Ordenacao_de_tres_numeros.c
+++ ex basicos/Ordenacao_de_tres_numeros.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
-int main()
-{
-    int n;
+void trocar(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-    printf("Digite um numero: ");
-    scanf("%d", &n);
+int main() {
+    int a, b, c;
 
-    
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &a);
 
+    printf("Digite o segundo numero: ");
+    scanf("%d", &b);
 
+    printf("Digite o terceiro numero: ");
+    scanf("%d", &c);
+
+    if (a > b) trocar(&a, &b);
+    if (b > c) trocar(&b, &c);
+    if (a > b) trocar(&a, &b);
+
+    printf("Numeros em ordem crescente: %d, %d, %d\n", a, b, c);
 
     return 0;
 }
